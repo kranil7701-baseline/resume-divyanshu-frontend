64903438b77131648e4a57358a3621f645c8a53e
nikhil <nikhilch0078@gmail.com>
2026-02-10 15:26:37 +0530

    security: move secrets to environment variables

--- config.js
+++ config.js
@@ -1,6 +1,4 @@
 const production = true;
 
 export const API = production ? 'https://resume-divyanshu-backend.vercel.app' : 'http://localhost:8000'
-
-
-export const GOOGLE_CLIENT_ID = '235462551112-imhssuvj1if5jeebgc91cc0om79tn8u1.apps.googleusercontent.com'
+export const GOOGLE_CLIENT_ID = process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID || '235462551112-imhssuvj1if5jeebgc91cc0om79tn8u1.apps.googleusercontent.com'

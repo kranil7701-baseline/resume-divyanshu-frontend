82c22d30c25b82d143f24281a1d296a405241ef8
nikhil <nikhilch0078@gmail.com>
2026-02-10 14:56:28 +0530

    g0rwg5erg

--- config.js
+++ config.js
@@ -1,3 +1,6 @@
-// export const API = 'https://resume-divyanshu-backend.vercel.app/'
-export const API = 'http://localhost:8000'
+const production = true;
+
+export const API = production ? 'https://resume-divyanshu-backend.vercel.app' : 'http://localhost:8000'
+
+
 export const GOOGLE_CLIENT_ID = '235462551112-imhssuvj1if5jeebgc91cc0om79tn8u1.apps.googleusercontent.com'
